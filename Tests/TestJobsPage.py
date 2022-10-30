7814b91a5444c94cbbcab6209d9b4e525696769a
Luke Billard <lukebillard04@gmail.com>
2022-10-30 15:31:55 -0230

    testing selenium with jenkins locally

--- Tests/TestJobsPage.py
+++ Tests/TestJobsPage.py
@@ -9,7 +9,7 @@ def testJobsPage():
     driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
 
     # Take action on browser
-    driver.get("http://localhost:3000/MUNster/jobs")
+    driver.get("http://localhost:3000/MUNster")
 
 
     # Establish waiting strategy (wait until element exists on page and is interactable before testing)
@@ -18,30 +18,32 @@ def testJobsPage():
     # Find elements on page
     #
     # common nav bar elements
-    find_jobs_button = driver.find_element(by=By.CSS_SELECTOR, value="find-jobs")
-    find_companies_button = driver.find_element(by=By.CSS_SELECTOR, value="find-companies")
-    find_people_button = driver.find_element(by=By.CSS_SELECTOR, value="find-people")
-    post_job_button = driver.find_element(by=By.CSS_SELECTOR, value="post-job")
+    find_logo_button = driver.find_element(by=By.CLASS_NAME, value="logo")
+    #find_jobs_button = driver.find_element(by=By.CSS_SELECTOR, value="find-jobs")
+    #find_companies_button = driver.find_element(by=By.CSS_SELECTOR, value="find-companies")
+    #find_people_button = driver.find_element(by=By.CSS_SELECTOR, value="find-people")
+    #post_job_button = driver.find_element(by=By.CSS_SELECTOR, value="post-job")
 
     # page specific elements
-    keyword_search_box = driver.find_element(by=By.NAME, value="keyword-search")
-    location_search_box = driver.find_element(by=By.NAME, value="location-search")
-    search_button = driver.find_element(by=By.CSS_SELECTOR, value="search")
+    #keyword_search_box = driver.find_element(by=By.NAME, value="keyword-search")
+    #location_search_box = driver.find_element(by=By.NAME, value="location-search")
+    #search_button = driver.find_element(by=By.CSS_SELECTOR, value="search")
 
     # add elements for posted jobs once implemented
 
     # Test elements
     #
     # common nav bar tests
-    find_jobs_button.click()
-    find_companies_button.click()
-    find_people_button.click()
-    post_job_button.click()
+    find_logo_button.click()
+    #find_jobs_button.click()
+    #find_companies_button.click()
+    #find_people_button.click()
+    #post_job_button.click()
 
     # page specific tests
-    keyword_search_box.send_keys("Test keyword search")
-    location_search_box.send_keys("Test location search")
-    search_button.click()
+    #keyword_search_box.send_keys("Test keyword search")
+    #location_search_box.send_keys("Test location search")
+    #search_button.click()
 
     # add tests for posted jobs once implemented
 
