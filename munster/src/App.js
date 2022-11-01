4c7b3eb44f7988808b1e8b60eba0b1606a89b215
LAPTOP-0CCDK2MI\madis <mhayley19@mun.ca>
2022-11-01 00:31:20 -0230

    fixed issue in app.js

--- munster/src/App.js
+++ munster/src/App.js
@@ -4,14 +4,11 @@ import NavBar from "./components/NavBar";
 import People from "./components/Pages/People"
 import Home from "./components/Pages/Home"
 import Applicants from "./components/Pages/Applicants";
-<<<<<<< HEAD
 import JobDetails from "./components/Pages/JobDetails";
 import JobApplication from "./components/JobApplication"
-=======
 import Login from "./components/Pages/Login";
 import Creation from "./components/Pages/Creation";
 import Profile from "./components/Pages/Profile";
->>>>>>> a79b194eeae36ee2ee22f42318ca68dc18569131
 
 function App() {
     return (
@@ -23,14 +20,11 @@ function App() {
             <Route path='MUNster' element={<Home />} />
             <Route path='/MUNster/people' element={<People />} />
             <Route path='/MUNster/applicants' element={<Applicants />} />
-<<<<<<< HEAD
             <Route path='/MUNster/jobs/details' element={<JobDetails />} />
             <Route path='/MUNster/jobs/apply' element={<JobApplication />}/>
-=======
             <Route path='/MUNster/login' element={<Login />} />
             <Route path='/MUNster/createProfile' element={<Creation/>} />
             <Route path="/MUNster/profile" element={<Profile/>} />
->>>>>>> a79b194eeae36ee2ee22f42318ca68dc18569131
           </Routes>
         </Router>
 
